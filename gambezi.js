2021eb4e1fefdd4c9f0b86a005b0cc6e4cf76b37
tigerh <tigerrun1998@gmail.com>
2017-07-20 19:39:47 -0400

    Code cleanup.

--- gambezi.js
+++ gambezi.js
@@ -1,161 +1,149 @@
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 function Gambezi(host_address) {
 	// Constructor
 	var m_object = this;
 	var m_websocket = new WebSocket("ws://" + host_address, "gambezi-protocol");
 	m_websocket.binaryType = 'arraybuffer';
-
-	var m_queued_ids = [];
+	var m_key_request_queue = [];
 	var m_root_node = new Node("", null, m_object);
 
+	////////////////////////////////////////////////////////////////////////////////
 	// Recieving data from server
 	m_websocket.onmessage = function(event) {
-		var data = new Uint8Array(event.data);
-		switch(data[0]) {
+		var buffer = new Uint8Array(event.data);
+		switch(buffer[0]) {
+
+			////////////////////////////////////////
 			// ID request response from server
 			case 0:
 				// Extract binary key
-				var binary_key = new Array(data[1]);
+				var binary_key = new Array(buffer[1]);
 				for(var i = 0;i < binary_key.length;i++) {
-					binary_key[i] = data[i + 2];
+					binary_key[i] = buffer[i + 2];
 				}
 
 				// Extract name
 				var name = "";
-				for(var i = 0;i < data[binary_key.length + 2];i++) {
-					name += String.fromCharCode(data[binary_key.length + 3 + i]);
+				for(var i = 0;i < buffer[binary_key.length + 2];i++) {
+					name += String.fromCharCode(buffer[binary_key.length + 3 + i]);
 				}
 
 				// Get the matching node and set the ID
-				var node = m_root_node;
-				for(var i = 0;i < binary_key.length - 1;i++) {
-					node = node.getChildWithID(binary_key[i]);
-				}
-				node = node.getChildWithName(name);
-				node.setKey(binary_key);
+				var node = m_object.node_traverse(binary_key, true);
+				node = node.get_child_with_name(name);
+				node.set_key(binary_key);
 
 				// Get the next queued ID request
-				m_object.getQueueID();
-
+				m_object.process_key_request_queue();
 				break;
 
+			////////////////////////////////////////
 			// Value update from server
 			case 1:
 				// Extract binary key
-				var binary_key = new Array(data[1]);
+				var binary_key = new Array(buffer[1]);
 				for(var i = 0;i < binary_key.length;i++) {
-					binary_key[i] = data[i + 2];
+					binary_key[i] = buffer[i + 2];
 				}
 
-				// Extract data length
-				var data_length = (data[binary_key.length + 2] << 8) | (data[binary_key.length + 3]);
-
 				// Extract data
-				var value = new ArrayBuffer(data_length);
-				var value_view = new Uint8Array(value);
+				var data_length = (buffer[binary_key.length + 2] << 8) | (buffer[binary_key.length + 3]);
+				var data_raw = new ArrayBuffer(data_length);
+				var data = new Uint8Array(data_raw);
 				for(var i = 0;i < data_length;i++) {
-					value_view[i] = data[binary_key.length + 4 + i];
+					data[i] = buffer[binary_key.length + 4 + i];
 				}
 
 				// Get the matching node and set the data
-				var node = m_root_node;
-				for(var i = 0;i < binary_key.length;i++) {
-					node = node.getChildWithID(binary_key[i]);
-				}
-				node.setData(value_view);
-				if(node.onDataRecieved) {
-					node.onDataRecieved(node);
-				}
+				var node = m_object.node_traverse(binary_key);
+				node.set_data(data);
 
+				// Callback if present
+				if(node.on_data_recieved) {
+					node.on_data_recieved(node);
+				}
 				break;
 		}
 	}
 
-	this.getID = function(parentKey, name) {
-		// Check if ID already assigned
-		var node = m_root_node;
-		for(var i = 0;i < parentKey.length;i++) {
-			node = node.getChildWithID(parentKey[i]);
-		}
-		node = node.getChildWithName(name);
-
-		// Bail if the node already has an ID
-		if(node.getID() >= 0) {
-			return;
-		}
-
+	////////////////////////////////////////////////////////////////////////////////
+	this.request_id = function(parent_key, name) {
 		// Create buffer
-		var buffer = new ArrayBuffer(parentKey.length + name.length + 3);
-		var view = new Uint8Array(buffer);
+		var buffer_raw = new ArrayBuffer(parent_key.length + name.length + 3);
+		var buffer = new Uint8Array(buffer_raw);
 
 		// Header
-		view[0] = 0x00;
+		buffer[0] = 0x00;
 
 		// Parent key
-		view[1] = parentKey.length;
-		for(var i = 0;i < parentKey.length;i++) {
-			view[i + 2] = parentKey[i];
+		buffer[1] = parent_key.length;
+		for(var i = 0;i < parent_key.length;i++) {
+			buffer[i + 2] = parent_key[i];
 		}
 
 		// Name
-		view[2 + parentKey.length] = name.length;
+		buffer[2 + parent_key.length] = name.length;
 		for(var j = 0;j < name.length;j++) {
-			view[j + 3 + parentKey.length] = name.charCodeAt(j);
+			buffer[j + 3 + parent_key.length] = name.charCodeAt(j);
 		}
 
 		// Send packet
-		m_websocket.send(buffer);
+		m_websocket.send(buffer_raw);
 	}
 
-	this.addQueueID = function(stringKey) {
-		// Check if an ID is already present
+	////////////////////////////////////////////////////////////////////////////////
+	// Also adds all parent keys to the queue if needed
+	// Returns the final node
+	this.add_key_to_request_queue = function(string_key) {
+		// Traverse up to final node
 		var node = m_root_node;
-		for(var i = 0;i < stringKey.length;i++) {
-			node = node.getChildWithName(stringKey[i]);
-		}
+		for(var i = 0;i < string_key.length;i++) {
+			// Go down one level
+			node = node.get_child_with_name(string_key[i]);
 
-		// Queue up ID request if needed
-		if(node.getID() < 0) {
-			m_queued_ids.push(stringKey);
+			// Queue up ID request if needed
+			if(node.get_id() < 0) {
+				m_key_request_queue.push(string_key.slice(0, i + 1));
+			}
 		}
-	}
-
-	this.getQueueID = function() {
-		// Bail if there are no more queued ID requests
-		if(m_queued_ids.length <= 0) {
-			return;
-		}
-
-		// Build the parent key
-		var stringKey = m_queued_ids.shift();
-		var parentKey = [];
-		var node = m_root_node;
-		for(var i = 0;i < stringKey.length - 1;i++) {
-			node = node.getChildWithName(stringKey[i]);
-			parentKey.push(node.getID());
-		}
-
-		// Request the ID
-		var name = stringKey[stringKey.length - 1];
-		m_object.getID(parentKey, name);
-	}
-
-	this.registerKey = function(stringKey) {
-		// Get the node
-		var node = m_root_node;
-		for(var i = 0;i < stringKey.length;i++) {
-			node = node.getChildWithName(stringKey[i]);
-		}
-
-		// Queue up the ID requests
-		for(var i = 0;i < stringKey.length;i++) {
-			m_object.addQueueID(stringKey.slice(0, i + 1));
-		}
-		m_object.getQueueID();
 
 		return node;
 	}
 
-	this.setValueRaw = function(key, data, offset, length) {
+	////////////////////////////////////////////////////////////////////////////////
+	this.process_key_request_queue = function() {
+		// Bail if there are no more queued ID requests
+		if(m_key_request_queue.length <= 0) {
+			return;
+		}
+
+		// Build the parent key
+		var string_key = m_key_request_queue.shift();
+		var parent_binary_key = new Array(string_key.length - 1);
+		var node = m_root_node;
+		for(var i = 0;i < string_key.length - 1;i++) {
+			node = node.get_child_with_name(string_key[i]);
+			parent_binary_key[i] = node.get_id();
+		}
+
+		// Request the ID
+		var name = string_key[string_key.length - 1];
+		m_object.request_id(parent_binary_key, name);
+	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	this.register_key = function(string_key) {
+		// Queue up the ID requests and get the node
+		var node = m_object.add_key_to_request_queue(string_key);
+		// Get an IDs necessary
+		m_object.process_key_request_queue();
+		// Return
+		return node;
+	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	this.set_data_raw = function(key, data, offset, length) {
 		// Create buffer
 		var buffer = new ArrayBuffer(key.length + length + 4);
 		var view = new Uint8Array(buffer);
@@ -183,7 +171,8 @@ function Gambezi(host_address) {
 		m_websocket.send(buffer);
 	}
 
-	this.requestValue = function(key, get_children) {
+	////////////////////////////////////////////////////////////////////////////////
+	this.request_data = function(key, get_children) {
 		// Create buffer
 		var buffer = new ArrayBuffer(key.length + 3);
 		var view = new Uint8Array(buffer);
@@ -202,9 +191,30 @@ function Gambezi(host_address) {
 		m_websocket.send(buffer);
 	}
 
+	////////////////////////////////////////////////////////////////////////////////
+	this.node_traverse = function(binary_key, get_parent) {
+		var node = m_root_node;
+		for(var i = 0;i < binary_key.length - (!!get_parent ? 1 : 0);i++) {
+			node = node.get_child_with_id(binary_key[i]);
+		}
+		return node;
+	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	this.node_traverse_by_name = function(string_key, get_parent) {
+		var node = m_root_node;
+		for(var i = 0;i < string_key.length - (!!get_parent ? 1 : 0);i++) {
+			node = node.get_child_with_name(string_key[i]);
+		}
+		return node;
+	}
 }
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 function Node(name, parent_key, parent_gambezi) {
+
+	////////////////////////////////////////////////////////////////////////////////
 	// Constructor
 	var m_object = this;
 	var m_name = name;
@@ -219,36 +229,44 @@ function Node(name, parent_key, parent_gambezi) {
 		m_key.push(-1);
 	}
 
-	this.getID = function() {
+	////////////////////////////////////////////////////////////////////////////////
+	this.get_id = function() {
 		return m_key[m_key.length - 1];
 	}
 
-	this.getName = function() {
+	////////////////////////////////////////////////////////////////////////////////
+	this.get_name = function() {
 		return m_name;
 	}
 
-	this.setKey = function(key) {
+	////////////////////////////////////////////////////////////////////////////////
+	this.set_key = function(key) {
 		m_key = key;
 	}
 
-	this.getKey = function() {
+	////////////////////////////////////////////////////////////////////////////////
+	this.get_key = function() {
 		return m_key;
 	}
 
-	this.setData = function(data) {
+	////////////////////////////////////////////////////////////////////////////////
+	this.set_data = function(data) {
 		m_data = data;
 	}
 
-	this.getData = function() {
+	////////////////////////////////////////////////////////////////////////////////
+	this.get_data = function() {
 		return m_data;
 	}
 
-	this.onDataRecieved = null;
+	////////////////////////////////////////////////////////////////////////////////
+	this.on_data_recieved = null;
 
-	this.getChildWithName = function(name) {
+	////////////////////////////////////////////////////////////////////////////////
+	this.get_child_with_name = function(name) {
 		// See if child already exists
 		for(var i = 0;i < m_children.length;i++) {
-			if(m_children[i].getName() == name) {
+			if(m_children[i].get_name() == name) {
 				return m_children[i];
 			}
 		}
@@ -259,10 +277,11 @@ function Node(name, parent_key, parent_gambezi) {
 		return child;
 	}
 
-	this.getChildWithID = function(id) {
+	////////////////////////////////////////////////////////////////////////////////
+	this.get_child_with_id = function(id) {
 		// See if child already exists
 		for(var i = 0;i < m_children.length;i++) {
-			if(m_children[i].getID() == id) {
+			if(m_children[i].get_id() == id) {
 				return m_children[i];
 			}
 		}
@@ -271,24 +290,20 @@ function Node(name, parent_key, parent_gambezi) {
 		return null;
 	}
 
-	this.setValueRaw = function(data, offset, length) {
-		m_gambezi.setValueRaw(m_object.getKey(), data, offset, length);
+	////////////////////////////////////////////////////////////////////////////////
+	this.set_data_raw = function(data, offset, length) {
+		m_gambezi.set_data_raw(m_object.get_key(), data, offset, length);
 	}
 
-	this.requestValue = function(get_children) {
-		m_gambezi.requestValue(m_object.getKey(), get_children);
+	////////////////////////////////////////////////////////////////////////////////
+	this.request_data = function(get_children) {
+		m_gambezi.request_data(m_object.get_key(), get_children);
 	}
 }
 
-function arrayCompare(a, b) {
-	if(a.length != b.length) return false;
-	for(var i = 0;i < a.length;i++)
-		if(a[i] != b[i]) return false;
-	return true;
-}
-
-gambezi = new Gambezi("127.0.0.1:7685");
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+gambezi = new Gambezi("127.0.0.1:7686");
 data = new ArrayBuffer(5)
 dataView = new Uint8Array(data)
 for(var i = 0;i < 5;i++) { dataView[i] = i; }
-//node = gambezi.registerKey(['hi'])
